6238482d9ad11a7382f931c254909aafbdd7a5c5
Backend Simplified <backendsimplified4u@gmail.com>
2024-01-31 11:54:05 +0200

    set sequelize

--- app/index.js
+++ app/index.js
@@ -1,11 +1,22 @@
 import app from "./app.js";
+import { sequelize } from "./database/dbInit.js";
 
 const PORT = 3000;
+
+async function assertDBConnection() {
+  try {
+    await sequelize.authenticate();
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 async function init() {
   console.log("Starting the app");
 
   try {
+    await assertDBConnection();
+    await sequelize.sync({ force: false }); // Starting with True, and after first creation - change it.
     app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
   } catch (error) {
     console.log(error);
